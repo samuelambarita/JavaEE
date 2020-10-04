14daf0418957e74a3e8b45e3cfd7ef58ac979caa
agnesgultom <agnesgultom9@gmail.com>
2020-10-04 13:11:04 +0700

    registration

---
+++ src/main/java/del/ac/id/demo/PraktikumWeek4Application.java
@@ -0,0 +1,13 @@
+package del.ac.id.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumWeek4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumWeek4Application.class, args);
+	}
+
+}
---
+++ src/main/java/del/ac/id/demo/jpa/LoginRepository.java
@@ -0,0 +1,7 @@
+package del.ac.id.demo.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepository extends JpaRepository<Login, String> {
+	Login findByUsername(String username);
+}
